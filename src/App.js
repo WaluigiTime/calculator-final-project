2d2f47ba35a53598efd95980eb2b3b3c3a8e941c
WaluigiTime <43816974+WaluigiTime@users.noreply.github.com>
2022-05-04 18:48:27 -0400

    made calculator functions, and ONE button uh oh, back to the lab again

--- src/App.js
+++ src/App.js
@@ -3,8 +3,28 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>{<Buttons />}</h1>
     </div>
   );
 }
+
+function addFunc(x, y) {
+  return x + y;
+}
+
+function multiFunc(x, y) {
+  return x * y;
+}
+function subtractFunc(x, y) {
+  return x - y;
+}
+
+function divideFunc(x, y) {
+  return x / y;
+}
+
+function Buttons() {
+  return <button onClick> 1 </button>;
+}
+
+console.log(divideFunc(4, 5));
