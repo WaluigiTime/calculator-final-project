d80a3b7eda923933ae4de5b6956105db07532def
WaluigiTime <43816974+WaluigiTime@users.noreply.github.com>
2022-05-11 18:27:44 -0400

    made a basic calculator!!! but its not done technically its not the og project plan. we need MOAR

--- src/App.js
+++ src/App.js
@@ -1,30 +1,114 @@
 import "./styles.css";
-
+import React, { useState } from "react";
+import {
+  addFunc,
+  multiFunc,
+  subtractFunc,
+  divideFunc,
+  modFunc,
+  sqrrootFunc,
+  powFunc
+} from "./CalFunctions.js";
 export default function App() {
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
+  const [ans, setAns] = useState(0);
   return (
     <div className="App">
-      <h1>{<Buttons />}</h1>
+      <h1>
+        <Button
+          buttonLabel={"+"}
+          onClickHandler={() => {
+            setAns(addFunc(x, y));
+          }}
+        />
+
+        <Button
+          buttonLabel={"-"}
+          onClickHandler={() => {
+            setAns(subtractFunc(x, y));
+          }}
+        />
+
+        <Button
+          buttonLabel={"x"}
+          onClickHandler={() => {
+            setAns(multiFunc(x, y));
+          }}
+        />
+
+        <Button
+          buttonLabel={"/"}
+          onClickHandler={() => {
+            setAns(divideFunc(x, y));
+          }}
+        />
+
+        <Button
+          buttonLabel={"%"}
+          onClickHandler={() => {
+            setAns(modFunc(x, y));
+          }}
+        />
+
+        <Button
+          buttonLabel={"Sqrt X"}
+          onClickHandler={() => {
+            setAns(sqrrootFunc(x));
+          }}
+        />
+
+        <Button
+          buttonLabel={"Sqrt Y"}
+          onClickHandler={() => {
+            setAns(sqrrootFunc(y));
+          }}
+        />
+
+        <Button
+          buttonLabel={"X^y"}
+          onClickHandler={() => {
+            setAns(powFunc(x, y));
+          }}
+        />
+
+        <div>
+          <Button
+            buttonLabel={"Clear"}
+            onClickHandler={() => {
+              setAns(0);
+              setX(0);
+              setY(0);
+            }}
+          />
+        </div>
+        <div>
+          x{" "}
+          <input
+            type="number"
+            value={x}
+            onChange={(e) => {
+              setX(parseFloat(e.target.value));
+            }}
+          />
+        </div>
+        <div>
+          y{" "}
+          <input
+            type="number"
+            value={y}
+            onChange={(e) => {
+              setY(parseFloat(e.target.value));
+            }}
+          />
+        </div>
+      </h1>
+
+      <div>{ans}</div>
     </div>
   );
 }
 
-function addFunc(x, y) {
-  return x + y;
+function Button(props) {
+  return <button onClick={props.onClickHandler}> {props.buttonLabel} </button>;
 }
-
-function multiFunc(x, y) {
-  return x * y;
-}
-function subtractFunc(x, y) {
-  return x - y;
-}
-
-function divideFunc(x, y) {
-  return x / y;
-}
-
-function Buttons() {
-  return <button onClick> 1 </button>;
-}
-
-console.log(divideFunc(4, 5));
--- src/CalFunctions.js
+++ src/CalFunctions.js
@@ -1,5 +1,28 @@
 import React from "react";
 
-function CalFunctions() {}
+export function addFunc(x, y) {
+  return x + y;
+}
 
-export default CalFunctions;
+export function multiFunc(x, y) {
+  return x * y;
+}
+export function subtractFunc(x, y) {
+  return x - y;
+}
+
+export function divideFunc(x, y) {
+  return x / y;
+}
+
+export function modFunc(x, y) {
+  return x % y;
+}
+
+export function sqrrootFunc(x, y) {
+  return Math.sqrt(x);
+}
+
+export function powFunc(x, y) {
+  return Math.pow(x, y);
+}
